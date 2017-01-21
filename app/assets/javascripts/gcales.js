86650c05516c2b287033a52f195acf88c2e669d4
tomomiik <tomomiik0210@gmail.com>
2017-01-21 09:51:47 +0000

    frontコミット

--- app/assets/javascripts/gcales.js
+++ app/assets/javascripts/gcales.js
@@ -5,9 +5,3 @@ $(function() {
     $('#tag-field').append('<input type="text" name="projects_description[]" id="projects_" class="form-control" placeholder="説明を入力"/>');
   });
 });
-
-
-$(".nav li a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
